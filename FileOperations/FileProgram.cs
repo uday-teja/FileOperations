0a29b1cef89ef988bc85ae2853ba153324a85e6e
udayteja.g <udayteja.g@UDAYTEJAG-PC>
2019-03-18 20:06:12 +0530

    Updated the code

---
+++ FileOperations/FileProgram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace FileOperations
+{
+    public class FileProgram
+    {
+        public static void Main(string[] args)
+        {
+            DirectoryInfo source = new DirectoryInfo(@"E:\Test\SourceFile");
+            DirectoryInfo destination = new DirectoryInfo(@"E:\Test\DestinationFile");
+            Operations operations = new Operations();
+            Console.WriteLine("What u want to do.. ??");
+            Console.WriteLine("1. Copy\n2. Move\n3. List");
+
+            int selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    operations.CopyDirectory(source, destination,"copy");
+                    Console.WriteLine("Copied Successfuly...!!!!");
+                    break;
+                case 2:
+                    operations.MoveDirectory(source, destination,"move");
+                    Console.WriteLine("Moved Successfuly...!!!!");
+                    break;
+                case 3:
+                    Console.WriteLine("\n-----File Directory-----\n");
+                    operations.ListDirectory(source);
+                    
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    break;
+            }
+        }
+    }
+}
---
+++ FileOperations/Operations.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileOperations
+{
+    public class Operations
+    {
+        // public List<FileInfo> fileList = new List<FileInfo>();
+        //enum FileOperations
+        //{
+        //    Copy,
+        //    Move,
+        //};
+
+        public void CheckDestination(DirectoryInfo destination)
+        {
+            if (!destination.Exists)
+            {
+                destination.Create();
+            }
+        }
+
+        public void CopyDirectory(DirectoryInfo source, DirectoryInfo destination, string task)
+        {
+            CheckDestination(destination);
+            FileInfo[] files = source.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                if (task == "copy")
+                {
+                    file.CopyTo(Path.Combine(destination.FullName, file.Name));
+                }
+                else if(task == "move")
+                {
+                    file.MoveTo(Path.Combine(destination.FullName, file.Name));
+                }
+             }
+            DirectoryInfo[] dirs = source.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                string destinationDir = Path.Combine(destination.FullName, dir.Name);
+                if(task == "move")
+                {
+                    dir.MoveTo(destinationDir);
+                }
+                CopyDirectory(dir, new DirectoryInfo(destinationDir),task);
+            }
+        }
+
+        public void MoveDirectory(DirectoryInfo source, DirectoryInfo destination, string task)
+        {
+            CheckDestination(destination);
+            CopyDirectory(source,destination,task);
+        }
+
+        public void ListDirectory(DirectoryInfo source)
+        {
+            FileInfo[] files = source.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(" " + file.Name);
+            }
+            DirectoryInfo[] dirs = source.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                Console.WriteLine(dir.Name);
+
+                string sourceDir = Path.Combine(source.FullName, dir.Name);
+                ListDirectory(new DirectoryInfo(sourceDir));
+            }
+        }
+    }
+}
